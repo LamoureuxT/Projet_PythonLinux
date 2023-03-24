8e61312f1a4e1f112049d31ec6ff84388484eeae
LamoureuxT <105443618+LamoureuxT@users.noreply.github.com>
2023-03-25 00:07:34 +0100

    Update Python_Linux.py

--- Python_Linux.py
+++ Python_Linux.py
@@ -60,8 +60,8 @@ app.layout = html.Div([
    ]), 
    html.Div([
        html.Div(className='table-container', children=generate_table(),            
-                style={'border': '1px solid #ddd', 'border-radius': '10px', 'margin-top': '20px', 'flex': '1','height': '50px',
-        'width': '70%'})       
+                style={'border': '1px solid #ddd', 'border-radius': '10px', 'margin-top': '20px', 'flex': '1','height': '100px',
+        'width': '100%'})       
    ])], style={"display": "flex", "flex-direction": "row", "margin-top": "20px"}), 
     
     html.Div(dcc.Graph(id='example-graph',
