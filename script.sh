6599425d334ec2d31a72022c39830034fc2f6995
LamoureuxT <105443618+LamoureuxT@users.noreply.github.com>
2023-03-26 22:45:36 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,4 @@
+## Script pour récupérer la valeur du bitcoin sur coingecko.com
+
 echo "$(date +'%Y-%m-%d %H:%M:%S'),
 $(curl -s 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false' | grep -oE '"current_price":[0-9]+(\.[0-9]+)?' | grep -oE '[0-9]+(\.[0-9]+)?')" >>/home/ec2-user/Projet/bitcoin_price.csv
